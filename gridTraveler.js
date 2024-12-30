4fc9e256f884c222a00db314c0a3d03d1ad679db
Asgar_George <asgarantony@gmail.com>
2024-12-30 21:08:05 +0530

    grid traveler

--- gridTraveler.js
+++ gridTraveler.js
@@ -2,16 +2,16 @@
 
 const gridTraveler = (m,n,memo = {}) => {
     const key = m + ',' + n;
+
+    if(key in memo) return memo[key]; // return this value if the value is already computed
+
+    if(m == 1 && n == 1) return 1;
+    if(m == 0 || n == 0) return 0;
     
-  if(m == 1 && n == 1) return 1;
-  if(m == 0 || n == 0) return 0;
-
-  if(key in memo) return memo[key]; // return this value if the value is already computed
-
-  let down = gridTraveler(m-1,n,memo);
-  let right = gridTraveler(m,n-1,memo);
+    let down = gridTraveler(m-1,n,memo);
+    let right = gridTraveler(m,n-1,memo);
   return memo[key] = down + right;
 };
 
-console.log(gridTraveler(2,3));
-console.log(gridTraveler(18,18));
+console.log(gridTraveler(18,18,memo = {}));
+console.log(memo)
