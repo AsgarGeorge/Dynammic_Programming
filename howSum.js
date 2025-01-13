8fa3a126c3925ebc4db102b7dcdf41ac3839d69b
Asgar_George <asgarantony@gmail.com>
2025-01-13 12:28:25 +0530

    check commit

---
+++ howSum.js
@@ -0,0 +1,13 @@
+/*Write a function "howSum(targetSum, numbers) that takes in a
+ targetSum and an array of numbers as arguments.
+The function should return an array containing any combination of
+ elements that add up to exactly the targetSum. If there is no
+combination that adds up to the targetSum, then return null. */
+
+const howSum = (targetSum, numbers) => {
+   
+};
+
+const result = howSum(7, [2, 3,1]); // [3, 2, 2]
+const result2 = howSum(8, [5, 3, 4, 7]); // [5, 3]
+console.log(result);
