c3f6a9a901dd4badfb761dc0b5e339f5efc8ec08
Asgar_George <asgarantony@gmail.com>
2025-04-20 21:17:18 +0530

    adding files

---
+++ My_Spring_DB.sql
@@ -0,0 +1,39 @@
+SELECT * FROM LEADS;
+SELECT * FROM CUSTOMER_DETAILS;
+SELECT * FROM TICKET;
+SELECT * FROM APP_USER;
+
+SELECT join_date FROM customer_details FETCH FIRST 10 ROWS ONLY;
+
+
+DROP TABLE Customer_Details;
+DROP TABLE TICKET;
+
+
+DESC CUSTOMER_DETAILS;
+
+
+SELECT 
+    TO_CHAR(TO_DATE(join_date, 'DD/MM/YYYY'), 'YYYY-MM') AS month,
+    COUNT(*) AS count
+FROM 
+    customer_details
+WHERE 
+    TO_DATE(join_date, 'DD/MM/YYYY') >= ADD_MONTHS(TRUNC(SYSDATE), -5)
+GROUP BY 
+    TO_CHAR(TO_DATE(join_date, 'DD/MM/YYYY'), 'YYYY-MM')
+ORDER BY 
+    month;
+    
+   
+BEGIN
+   FOR t IN (SELECT table_name FROM user_tables) LOOP
+      EXECUTE IMMEDIATE 'DROP TABLE "' || t.table_name || '" CASCADE CONSTRAINTS';
+   END LOOP;
+END;
+/
+
+
+
+
+
